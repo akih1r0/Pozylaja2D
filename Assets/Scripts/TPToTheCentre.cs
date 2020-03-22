06289d493e099fc074c7467dbff87e9e3bff4b2b
Maksym Vorobiov <mksmfaust@gmail.com>
2020-03-22 18:10:59 +0100

    Respawn created

---
+++ Assets/Scripts/TPToTheCentre.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPToTheCentre : MonoBehaviour
+{
+    public GameObject respawn;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ball")
+        {
+            collision.transform.position = respawn.transform.position;
+        }
+    }
+}
